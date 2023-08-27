1ff1b19c0fc159d95b24d5de409fe96fa0461cc7
tientcdtk5 <manhtien150799@gmail.com>
2023-08-27 20:45:16 +0700

    Add new project food

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chào Vân Anh!',
+    introDesc: `Tớ có câu hỏi dành cho cậu!`,
+    btnIntro: '??????',
+    title: 'phải chăng cậu đã thích tớ :)))',
+    desc: ' ',
+    btnYes: 'đúng rồi <3',
+    btnNo: 'Không',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích tớ <3',
+    btnReply: 'Gửi',
+    reply: 'Tại vì đó là cậu :v',
+    mess: 'okkkk !!!',
+    messDesc: 'hẹn cậu tuần sau đi chơi nhá!!!',
+    btnAccept: 'Okii',
+    messLink: 'https://www.facebook.com/profile.php?id=100011409428182' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
